997e63cd65f0414647ec118be53df9399f4efaff
Ajith Arasu <ajith@securra.com>
2025-01-28 19:41:44 +0530

    update letsbegin nav

--- src/components/heroSection/index.js
+++ src/components/heroSection/index.js
@@ -3,24 +3,26 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="flex-row">
-    <div className="apple-computers" />
-    <div className="frame">
-      <div className="innovating-for-better-lives">
-        <span className="innovating">Innovating</span>
-        <span className="for-better-lives"> for Better Lives</span>
-      </div>
-      <div className="frame-1">
-        <span className="empowering-productivity">
-          Empowering Productivity | Fostering Creativity | Enhancing Safety
-        </span>
-        <div className="div-2">
-          <div className="button-3">
-            <span className="lets-start">Let’s start</span>
+      <div className="apple-computers" />
+      <div className="frame">
+        <div className="innovating-for-better-lives">
+          <span className="innovating">Innovating</span>
+          <span className="for-better-lives"> for Better Lives</span>
+        </div>
+        <div className="frame-1">
+          <span className="empowering-productivity">
+            Empowering Productivity | Fostering Creativity | Enhancing Safety
+          </span>
+          <div className="div-2">
+            <div className="button-3">
+              <a href="#contact-us" className="lets-start">
+                Let’s start
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
